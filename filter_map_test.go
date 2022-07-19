a518808df200832def81861c3332b842d322a83e
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2022-07-19 17:46:22 +0100

    feat(FilterMap): combine filtration and mapping in a single operation

---
+++ filter_map_test.go
@@ -0,0 +1,20 @@
+package fungi
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFilterMap(t *testing.T) {
+	source := SliceStream([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	filterMap := FilterMap(oddPlusOne)
+	stream := filterMap(source)
+	result, err := CollectSlice(stream)
+	assert.NoError(t, err)
+	assert.Equal(t, []int{2, 4, 6, 8, 10}, result)
+}
+
+func oddPlusOne(i int) (int, bool) {
+	return i + 1, i%2 != 0
+}
