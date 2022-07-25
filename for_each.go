e2709b8674deef881d87f0592b30e99e959d9cb0
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2022-07-25 19:31:35 +0100

    feat(ForEach): handle errors generated by closure

--- for_each.go
+++ for_each.go
@@ -4,12 +4,12 @@ import "io"
 
 // ForEach uses the custom handler function given to it to process every item
 // from a stream.
-func ForEach[T any](handle func(T)) StreamHandler[T] {
+func ForEach[T any](handle func(T) error) StreamHandler[T] {
 	return func(items Stream[T]) (err error) {
 		var item T
 		for err == nil {
 			if item, err = items.Next(); err == nil {
-				handle(item)
+				err = handle(item)
 			}
 		}
 		if err == io.EOF {
--- for_each_test.go
+++ for_each_test.go
@@ -1,6 +1,7 @@
 package fungi
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,8 +10,22 @@ import (
 func TestForEach(t *testing.T) {
 	source := SliceStream([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
 	accumulator := 0
-	adder := ForEach(func(i int) { accumulator += i })
+	adder := ForEach(func(i int) error { accumulator += i; return nil })
 	err := adder(source)
 	assert.NoError(t, err)
 	assert.Equal(t, 55, accumulator)
+}
+
+func TestForEachWithErrorDuringHandling(t *testing.T) {
+	source := SliceStream([]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
+	loop := ForEach(errorMaker)
+	err := loop(source)
+	assert.Error(t, err)
+}
+
+func errorMaker(i int) error {
+	if i == 6 {
+		return errors.New(":(")
+	}
+	return nil
 }
