0539ba79634b6d8b9eb8f9915bc5f64472f4b9bd
Viktor A. Rozenko Voitenko <sharp.vik@gmail.com>
2024-10-08 18:32:25 +0300

    feat: add EmptyStream

--- errstream.go
+++ errstream.go
@@ -1,7 +1,13 @@
 package fungi
 
+import "io"
+
 func ErrorStream[T any](err error) Stream[T] {
 	return &errorStream[T]{err}
+}
+
+func EmptyStream[T any]() Stream[T] {
+	return ErrorStream[T](io.EOF)
 }
 
 type errorStream[T any] struct {
